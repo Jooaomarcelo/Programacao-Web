c0e310d16d6d71ab7063587bd47c9ea604ac2a5d
João Marcelo <JoaoMdSB@hotmail.com>
2024-10-04 18:10:17 -0300

    added jogo-da-adivinhacao

--- jogo-da-advinhacao/js/index.js
+++ jogo-da-advinhacao/js/index.js
@@ -16,7 +16,10 @@ let inicio = null;
 let fim = null;
 let numRandom = null;
 let numPensado = null;
-let chances = null;
+
+// Chances
+const tentativaParagrafo = document.querySelector( '#chances' );
+let chances = 3;
 
 btnPensar.addEventListener('click', () => {
   // Recebendo os valores d einicio e fim
@@ -38,13 +41,40 @@ btnPensar.addEventListener('click', () => {
 btnAdivinhar.addEventListener('click', () => {
   numPensado = Number(inputPensado.value);
 
-  if(!Number.isInteger(numPensado) || numPensado < inicio || numPensado > fim) {
-    saida.textContent = `Digite um número inteiro positivo entre ${inicio} e ${fim}.`
-  } else if(numPensado == numRandom) {
-    saida.textContent = `Parabéns! Você sabe mesmo ler mentes.`;
-    return;
-  } else {
-    saida.textContent = `Que pena! Tente novamente`;
-    return;
+  if(chances > 1) {
+    if(!Number.isInteger(numPensado) || numPensado < inicio || numPensado > fim) {
+      saida.textContent = `Digite um número inteiro positivo entre ${inicio} e ${fim}.`
+    }
+
+    if(numPensado === numRandom) {
+      saida.textContent = `Parabéns! Você sabe mesmo ler mentes.`;
+      reset();
+      return;
+    } else if (numPensado > numRandom) {
+      saida.textContent = `Pense em um número menor!`;
+      tentativaParagrafo.textContent = `Número de tentativas: ${--chances}`;
+    } else {
+      saida.textContent = `Pense em um número maior!`;
+      tentativaParagrafo.textContent = `Número de tentativas: ${--chances}`;
+    }
+
+  } else {  
+    alert(`Você perdeu! O número era ${numRandom}.`);
+    reset();
   }
 });
+
+function reset() {
+  inicio = null;
+  fim = null;
+  numRandom = null;
+  numPensado = null;
+  chances = 3;
+
+  inputInicio.value = '';
+  inputFim.value = '';
+  inputPensado.value = '';
+
+  saida.textContent = '';
+  tentativaParagrafo.textContent = '';
+};
